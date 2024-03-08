792f98d4ac11e605756e9ff5eb01a1bcdd03063d
ersylium <ersylium@gmail.com>
2024-03-08 19:49:04 +0800

    updated console_handler.py to comply with new template changes

--- console_handler.py
+++ console_handler.py
@@ -2,7 +2,7 @@ import math
 with open("prompt.txt", 'r') as file:
     prompt = file.read()
 
-def cout(freq: dict, filename: str):
+def cout(freq: dict, filename: str, mode: str):
     global prompt
     block = "|"
     output = ""
@@ -19,5 +19,6 @@ def cout(freq: dict, filename: str):
         output = output + str(key) + " "*(maxlen - len(str(key))) + ": " + block*(math.ceil(value/ratio)) + f" ({str(value)})" + "\n"
     prompt = prompt.replace("$filename", filename)
     prompt = prompt.replace("$status", "SUCCESS" if len(freq) > 0 else "FAIL")
+    prompt = prompt.replace("$mode", mode)
     print(prompt)
     print(output)
