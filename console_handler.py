de6869744dfe83dbf0fa7ce115a861a3298452f5
epicbuilder2007 <69185969+epicbuilder2007@users.noreply.github.com>
2024-03-08 19:43:44 +0800

    manually added the changes. fuck you gitbug

--- console_handler.py
+++ console_handler.py
@@ -1,3 +1,4 @@
+import math
 with open("prompt.txt", 'r') as file:
     prompt = file.read()
 
@@ -6,11 +7,16 @@ def cout(freq: dict, filename: str):
     block = "|"
     output = ""
     maxlen = 0
+    maxval = 0
     for key in freq.keys():
         if len(key) > maxlen:
             maxlen = len(key)
+    for val in freq.values():
+        if val > maxval:
+            maxval = val
+    ratio = maxval/50
     for key, value in freq.items():
-        output = output + str(key) + " "*(maxlen - len(str(key))) + ": " + block*(int(value)+1) + f" ({str(value)})" + "\n"
+        output = output + str(key) + " "*(maxlen - len(str(key))) + ": " + block*(math.ceil(value/ratio)) + f" ({str(value)})" + "\n"
     prompt = prompt.replace("$filename", filename)
     prompt = prompt.replace("$status", "SUCCESS" if len(freq) > 0 else "FAIL")
     print(prompt)
