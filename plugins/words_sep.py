bd9bebfac1859d7cdbf3ef506a5e4c2a07848a14
epicbuilder2007 <69185969+epicbuilder2007@users.noreply.github.com>
2024-03-20 11:57:35 +0800

    Update words_sep.py

--- plugins/words_sep.py
+++ plugins/words_sep.py
@@ -1,16 +1,5 @@
-def main(content: str, _):
-    para = content.split("\n")
-    words = []
-    for i in para:
-        if i != '':
-            wordlist = i.split(' ')
-            for j in range(len(wordlist)):
-                word = list(wordlist[j])
-                for k in range(len(word)):
-                    if not word[k].isalpha():
-                        word[k] = ''
-                newword = ''.join(word)
-                if newword != '':
-                    words += [newword]
+def main(content: str, mode):
+    modes = mode()
+    words = modes.words(content)
     print(words)
     return 0
